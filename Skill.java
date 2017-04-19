b0a8762439de904b460422d47eef97bdf176a786
TDavies95 <t.davies951@gmail.com>
2017-04-19 14:04:11 -0500

    Add files via upload (#4)

---
+++ Skill.java
@@ -0,0 +1,18 @@
+package Code.FRPG;
+
+public class Skill{
+    private int id;
+    public Skill(){
+        id = 0;
+    }
+    public Skill(int id){
+     this.id = id;
+    }
+    public void setSkill(int id){
+     this.id = id;   
+    }
+    public int getSkill(){
+     return id;   
+    }
+}
+    
