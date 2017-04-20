628ef24943f5596b503eace03f5536ccbe04b7da
TDavies95 <t.davies951@gmail.com>
2017-04-20 12:35:48 -0500

    Add files via upload

--- SourceFiles/Code/FRPG/Room.java
+++ SourceFiles/Code/FRPG/Room.java
@@ -1,5 +1,11 @@
 package Code.FRPG;
-
+import java.util.*;
 public class Room{
-    
+     
+ 
+
+    public static void main(String [] args){
+
+     
+    }
 }
---
+++ SourceFiles/Code/FRPG/SkillBook.java
@@ -0,0 +1,16 @@
+package Code.FRPG;
+
+public class SkillBook{
+    public static Skill getSkill(int id) {
+        Skill skl = new Skill();
+        if (id == 0) {
+      //      skl = getSkill();
+            
+        } else if (id == 1) {
+      //      skl = getSkill();
+            
+        }
+    return null;
+    
+    }
+}
